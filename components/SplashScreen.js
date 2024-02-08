ff0449710aaa23c06ab053148b493c4318ff3bf8
FraCanta <you@example.comfcantale14@gmail.com>
2024-02-08 15:23:14 +0100

    anime screen splash

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -1,5 +1,6 @@
 "use client";
-
+import localFont from "next/font/local";
+const myFont = localFont({ src: "../fonts/Tactico-Grunge.otf" });
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import anime from "animejs";
@@ -13,61 +14,72 @@ const SplashScreen = ({ finishLoading }) => {
 
     loader
       .add({
-        targets: "#logo",
+        targets: "#logo1",
         delay: 0,
-        y: -400,
-        duration: 500,
+        opacity: 1,
+        translateX: 0,
+        duration: 1000,
         easing: "easeInOutExpo",
       })
       .add({
-        targets: "#logo",
-        delay: 100,
-        scale: 1.25,
-        duration: 500,
+        targets: "#logo2",
+        delay: 2,
+        opacity: 1,
+        translateX: 0,
+        duration: 1200,
         easing: "easeInOutExpo",
       })
-      .add({
-        targets: "#logo",
-        delay: 100,
-        scale: 1,
-        duration: 500,
-        easing: "easeInOutExpo",
-      })
-      .add({
-        targets: "#logo",
-        delay: 100,
-        scale: 1.25,
-        duration: 500,
-        easing: "easeInOutExpo",
-      })
-      .add({
-        targets: "#logo",
-        delay: 100,
-        scale: 1,
-        duration: 500,
-        easing: "easeInOutExpo",
-      })
+
       .add({
         targets: "#payoff",
         delay: 100,
-        scale: 1,
-        duration: 500,
+        opacity: 0,
+        duration: 1000,
+        easing: "easeInOutExpo",
+      })
+      .add({
+        targets: "#logo",
+        delay: 100,
+        scale: 5,
+        opacity: 0,
+        duration: 1000,
         easing: "easeInOutExpo",
       });
   };
 
   useEffect(() => {
-    const timeout = setTimeout(() => setIsMounted(true), 10);
+    const timeout = setTimeout(() => setIsMounted(true), 20);
     animate();
     return () => clearTimeout(timeout);
   }, []);
 
   return (
     <div
-      className="flex h-screen items-center justify-center flex-col"
+      className="flex h-screen items-center justify-center flex-col overflow-hidden"
       isMounted={isMounted}
     >
-      <Image id="logo" src={Logo} alt="" width={600} height={600} />
+      {/* <Image
+        id="logo"
+        src={Logo}
+        alt=""
+        width={600}
+        height={600}
+        className="translate-y-[100px] opacity-0"
+      /> */}
+      <div className="flex" id="logo">
+        <span
+          className={`${myFont.className} text-[200px] text-[#368B90] opacity-0`}
+          id="logo1"
+        >
+          CO
+        </span>
+        <span
+          className={`${myFont.className} text-[200px] text-[#bb5471] opacity-0`}
+          id="logo2"
+        >
+          FACTORY
+        </span>
+      </div>
       <p id="payoff">Agenzia creativa di incontri x brands</p>
     </div>
   );
