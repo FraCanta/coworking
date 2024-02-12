85b1cce23e1fbd54f319d2d00f6a5a39888e91a5
FraCanta <you@example.comfcantale14@gmail.com>
2024-02-12 15:23:13 +0100

    blurry

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ import Line from "./Line/Line";
 const Hero = ({ children }) => {
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex min-h-[calc(90vh_-_60px)] w-[90%] flex-col gap-6 md:gap-10  mx-auto items-center justify-center">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[9999] flex min-h-[calc(90vh_-_60px)] w-[90%] flex-col gap-6 md:gap-10  mx-auto items-center justify-center">
         {children}
       </div>
       <BlurryLights />
