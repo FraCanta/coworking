f00f09cedbac2a9c15cd58096f8ab4e246feebea
FraCanta <you@example.comfcantale14@gmail.com>
2024-02-19 10:48:42 +0100

    menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,44 +1,48 @@
 "use client";
 
-import Link from "next/link";
 import React, { useState, useEffect } from "react";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { IoMenuOutline } from "react-icons/io5";
-import { RiCloseFill } from "react-icons/ri";
-import Line from "./Line/Line";
+import { motion } from "framer-motion";
+import Link from "next/link";
 import Logo from "@/public/assets/logo/logo.svg";
 import Image from "next/image";
 import DarkModeToggle from "./DarkModeToggle/DarkModeToggle";
 import { MenuButton } from "./MenuButton";
+import Line from "./Line/Line";
+
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [color, setColor] = useState("transparent");
-  // const [textColor, setTextColor] = useState("white");
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
   const [isOpen, setOpen] = useState(false);
-  const handleNav = () => {
-    setNav(!nav);
-  };
 
   useEffect(() => {
-    const changeColor = () => {
-      if (window.scrollY >= 90) {
-        setColor("#ffffff");
-        // setTextColor("#000000");
+    const handleScroll = () => {
+      const currentScrollTop = window.scrollY;
+
+      if (currentScrollTop > lastScrollTop) {
+        // Scrolling down
+        setIsVisible(false);
       } else {
-        setColor("transparent");
-        // setTextColor("#ffffff");
+        // Scrolling up
+        setIsVisible(true);
       }
+
+      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
     };
-    window.addEventListener("scroll", changeColor);
-  }, []);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
 
   return (
-    <div
-      style={{ backgroundColor: `${color}` }}
+    <motion.div
+      style={{ display: isVisible ? "block " : "none" }}
       className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
     >
-      <div className="w-[90%] m-auto flex justify-between items-center p-4 text-white">
-        <Link href="/" className="z-[20]">
+      <div className="w-[90%] m-auto flex justify-between items-center py-4 text-white">
+        <Link href="/" className="z-[20]" onClick={() => setOpen(false)}>
           <Image
             src={Logo}
             alt="logo"
@@ -49,18 +53,10 @@ const Navbar = () => {
         </Link>
         <div className="flex gap-4 items-center">
           <DarkModeToggle />
-          <div onClick={handleNav} className="block ease-in duration-300 z-20">
-            {/* {nav ? (
-              <RiCloseFill
-                size={40}
-                className="text-white dark:text-third cursor-pointer "
-              />
-            ) : (
-              <IoMenuOutline
-                size={40}
-                className="text-white dark:text-third cursor-pointer ease-in duration-300"
-              />
-            )} */}
+          <div
+            onClick={() => setOpen(!isOpen)}
+            className="block ease-in duration-300 z-20"
+          >
             <MenuButton
               isOpen={isOpen}
               onClick={() => setOpen(!isOpen)}
@@ -74,29 +70,28 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? "absolute top-0 left-0 right-0 bottom-0 flex items-center w-full h-screen bg-third dark:bg-white text-white dark:text-third  ease-in duration-300"
-              : "absolute top-0 left-[-100%] right-0 bottom-0 flex items-center w-full h-screen bg-third dark:bg-white text-white dark:text-third  ease-in duration-300 z-20"
-          }
+        <motion.div
+          initial={{ opacity: 0, x: "-100%" }}
+          animate={{ opacity: isOpen ? 1 : 0, x: isOpen ? "0%" : "-100%" }}
+          transition={{ ease: "easeOut", duration: 0.8 }}
+          className="absolute top-0 left-0 right-0 bottom-0 flex items-center w-full h-screen bg-third dark:bg-white text-white dark:text-third"
         >
           <div className="w-[90%] mx-auto grid grid-cols-1 lg:grid-cols-3">
             <ul className="h-full flex flex-col gap-6">
               <li
-                onClick={handleNav}
+                onClick={() => setOpen(false)}
                 className="text-[4rem] lg:text-[8rem] cursor-pointer transition hover:text-gray-500"
               >
                 <Link href="/factory">Factory</Link>
               </li>
               <li
-                onClick={handleNav}
+                onClick={() => setOpen(false)}
                 className="text-[4rem] lg:text-[8rem] cursor-pointer transition hover:text-gray-500"
               >
                 <Link href="/works">Works</Link>
               </li>
               <li
-                onClick={handleNav}
+                onClick={() => setOpen(false)}
                 className="text-[4rem] lg:text-[8rem] cursor-pointer transition hover:text-gray-500"
               >
                 <Link href="/contatti">Contact</Link>
@@ -105,12 +100,12 @@ const Navbar = () => {
             <div></div>
             <div>Frase</div>
           </div>
-        </div>
+        </motion.div>
       </div>
       <div className="w-[90vw] mx-auto">
         <Line />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
